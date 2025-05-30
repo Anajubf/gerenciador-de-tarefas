91bc1364a27a312890850dbeafe1715c563fb0ba
Ana Júlia <ana.j.ferreira15@aluno.senai.br>
2025-05-30 08:29:56 -0300

    Adiciona novas tarefas

---
+++ tarefas.js
@@ -0,0 +1,17 @@
+let tarefas = [
+  "Estudar JavaScript",
+  "Fazer exercícios de matemática",
+  "Lavar a louça",
+  "Comprar mercado",
+  "Ir à academia",
+  "Ler um livro",
+  "Fazer revisão para o teste",
+  "Limpar o quarto",
+  "Estudar para concurso",
+  "Organizar o computador",
+];
+
+console.log(`Antes do push ${tarefas}`);
+tarefas.push(`Estudar para a prova, Ler um livro.`);
+
+console.log(`Depois do push ${tarefas}`);
